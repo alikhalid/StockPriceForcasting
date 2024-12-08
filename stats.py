4d7b644891a62d56eac49dad94e09923b9d6e636
alikhalid <ali.kahn92@gmail.com>
2024-12-08 15:06:47 -0600

    compute prediction stats

--- stats.py
+++ stats.py
@@ -1,12 +1,29 @@
 import numpy as np
+import pandas as pd
 
-class Profit:
-    def __init__(self, time, prices, num_points):
-        self.time = time
-        self.prices = prices
 
-        self.num_points = int(min(len(prices) * 0.5, num_points))
-        self.profit = np.zeros(10)
+def compute_prediction_stats(tickers, pred, obj):
+    num_points = pred.shape[0]
+    num_stocks = len(tickers)
 
-    def compute(self, time, buy):
+    columns = ["Accuracy", "StdDevObj", "StdDevPred", "PredRealization", "RMSE"]
+    stats = np.zeros((num_stocks, len(columns)))
 
+    for stock_id in range(num_stocks):
+        _pred = np.array(pred[:, stock_id])
+        _obj = np.array(obj[:, stock_id])
+
+        accuracy = np.sum((_pred * _obj) > 0)/np.sum(_pred != 0)
+        std_dev_obj = np.sqrt(np.dot(_obj, _obj) / len(_obj))
+        std_dev_pred = np.sqrt(np.dot(_pred, _pred) / len(_pred))
+        realization = (np.dot(_pred, _obj) / len(_pred)) / std_dev_pred
+        rmse = np.sqrt(np.mean(np.square(_pred - _obj)))
+
+        stats[0, :] = np.array([accuracy, std_dev_obj, std_dev_pred, realization, rmse])
+
+    df = pd.DataFrame(stats, columns=columns)
+    df.insert(0, "Tickers", tickers)
+    return df
+
+def compute_profit_stats(tickers, pred, prices):
+    pass
