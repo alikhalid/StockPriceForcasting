9b0ab9e048ec366bad6b3f3c92087fbe1494384e
alikhalid <ali.kahn92@gmail.com>
2024-12-08 14:45:15 -0600

    predict n sparsely connected stocks

--- data_loader.py
+++ data_loader.py
@@ -6,7 +6,7 @@ import numpy as np
 
 def load_stock(ticker):
     ticker = yf.Ticker(ticker)
-    historical_data = ticker.history(period="10y")
+    historical_data = ticker.history(period="5y")
     close = np.array(historical_data.Close)
     open = np.array(historical_data.Open)
 
@@ -24,19 +24,19 @@ def load_stock(ticker):
     data = np.concatenate([[(close - open) / open],
                            [(next_open - close) / close],
                            [vol]])
-    return data.astype(np.float32), obj.astype(np.float32)
+    return data.astype(np.float32)[:, :1000], obj.astype(np.float32)[:1000]
 
 
 def setup_cov_tensors(X, y, sequence_length):
-    targets = y[:, sequence_length:] if len(y.shape) > 1 else y[sequence_length:]
+    targets = y[..., sequence_length:]
     inputs = []
     for i in range(y.shape[-1] - sequence_length):
-        inputs.append(X[:, i:i + sequence_length])
+        inputs.append(X[..., i:i + sequence_length])
 
     inputs = np.array(inputs)
 
     # Convert to PyTorch tensors
     inputs = torch.from_numpy(inputs)
-    targets = torch.from_numpy(targets)#.unsqueeze(1)
+    targets = torch.from_numpy(targets)
 
-    return inputs, targets
+    return inputs, targets.transpose(0, 1)
